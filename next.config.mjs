52ab5cd2dd59ab4e436570241029b3c2e579fabc
Tayfun Türkmen <156463434+TayfunTurkmen@users.noreply.github.com>
2025-01-13 15:07:17 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+module.exports = {
+  images: {
+    localPatterns: [
+      {
+        pathname: '/',
+        search: '',
+      },
+    ],
+  },
+}
+};
 
 export default nextConfig;
